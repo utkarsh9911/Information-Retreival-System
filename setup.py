626b4dbcfd2faed551c04d7819cc8fa3bd8b6d27
utkarsh9911 <utkarshraj7217@gmail.com>
2024-10-31 00:23:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Utkarsh',
+    author_email= 'utakrshraj7217@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
