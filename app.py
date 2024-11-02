52ea6fd3b95c83ea9a27abfdd36589acef821b59
utkarsh9911 <utkarshraj7217@gmail.com>
2024-11-03 00:17:36 +0530

    trial file updated

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+# from langchain_openai.chat_models import ChatOpenAI
+
+# st.title("🦜🔗 Information Retrieval System")
+def main():
+    st.set_page_config("Information Retrieval")
+    st.header("Information Retrieval System💁")
+
+
+
+
+
+if __name__=="__main__":
+    main()
